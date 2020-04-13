f24c637503a2732833e8b47c9e11dbdad3c14e8a
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-13 15:56:53 +0100

    Creates the crest api

--- scripts/module_06/create-api-gateway.js
+++ scripts/module_06/create-api-gateway.js
@@ -1,63 +1,79 @@
 // Imports
-const AWS = require('aws-sdk')
+const AWS = require('aws-sdk');
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' });
 
 // Declare local variables
-// TODO: Create api gateway object
-const apiName = 'hamster-api'
+const apiG = new AWS.APIGateway();
+const apiName = 'hamster-api';
 
-let apiData
+let apiData;
 
 createRestApi(apiName)
-.then((data) => {
-  apiData = data
-  return getRootResource(apiData)
-})
-.then(rootResourceId => createResource(rootResourceId, 'hbfl', apiData))
-.then(hbflResourceId => createResourceMethod(hbflResourceId, 'GET', apiData))
-.then(hbflResourceId => createMethodIntegration(hbflResourceId, 'GET', apiData))
-.then(hbflResourceId => createResource(hbflResourceId, '{proxy+}', apiData))
-.then(proxyResourceId => createResourceMethod(proxyResourceId, 'ANY', apiData, 'proxy'))
-.then(proxyResourceId => createMethodIntegration(proxyResourceId, 'ANY', apiData, 'proxy'))
-.then(data => console.log(data))
+  .then((data) => {
+    apiData = data;
+    return getRootResource(apiData);
+  })
+  .then((rootResourceId) => createResource(rootResourceId, 'hbfl', apiData))
+  .then((hbflResourceId) =>
+    createResourceMethod(hbflResourceId, 'GET', apiData),
+  )
+  .then((hbflResourceId) =>
+    createMethodIntegration(hbflResourceId, 'GET', apiData),
+  )
+  .then((hbflResourceId) => createResource(hbflResourceId, '{proxy+}', apiData))
+  .then((proxyResourceId) =>
+    createResourceMethod(proxyResourceId, 'ANY', apiData, 'proxy'),
+  )
+  .then((proxyResourceId) =>
+    createMethodIntegration(proxyResourceId, 'ANY', apiData, 'proxy'),
+  )
+  .then((data) => console.log(data));
 
-function createRestApi (apiName) {
-  // TODO: Create params const
+function createRestApi(apiName) {
+  const params = {
+    Name: apiName,
+  };
 
   return new Promise((resolve, reject) => {
-    // TODO: Create a new rest API
-  })
+    apiG.createRestApi(params, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
 }
 
-function getRootResource (api) {
+function getRootResource(api) {
   // TODO: Create params const
 
   return new Promise((resolve, reject) => {
     // TODO: Get the resources and find the resource with path '/'
-  })
+  });
 }
 
-function createResource (parentResourceId, resourcePath, api) {
+function createResource(parentResourceId, resourcePath, api) {
   // TODO: Create params const
 
   return new Promise((resolve, reject) => {
     // TODO: Create the resource and return the resource id
-  })
+  });
 }
 
-function createResourceMethod (resourceId, method, api, path) {
+function createResourceMethod(resourceId, method, api, path) {
   // TODO: Create params const
 
   return new Promise((resolve, reject) => {
     // TODO: Put the method and return the resourceId argument
-  })
+  });
 }
 
-function createMethodIntegration (resourceId, method, api, path) {
+function createMethodIntegration(resourceId, method, api, path) {
   // TODO: Create params const
 
   return new Promise((resolve, reject) => {
     // TODO: Put the integration and return the resourceId argument
-  })
+  });
 }
