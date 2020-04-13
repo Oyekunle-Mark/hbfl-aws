aaba09c68ba78058d7115f96aab1a77ad7107500
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-13 19:04:53 +0100

    Creates an sns subscription

--- scripts/module_09/create-sns-subscription.js
+++ scripts/module_09/create-sns-subscription.js
@@ -1,21 +1,30 @@
 // Imports
-const AWS = require('aws-sdk')
+const AWS = require('aws-sdk');
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' });
 
 // Declare local variables
-const sns = new AWS.SNS()
-const type = 'sms'
-const endpoint = '/* TODO: Add your mobile number with country code */'
-const topicArn = '/* TODO: Add your sns topic arn */'
+const sns = new AWS.SNS();
+const type = 'sms';
+const endpoint = '2348064286205';
+const topicArn = 'arn:aws:sns:us-east-1:188714396569:hamster-topic';
 
-createSubscription(type, topicArn, endpoint)
-.then(data => console.log(data))
+createSubscription(type, topicArn, endpoint).then((data) => console.log(data));
 
-function createSubscription (type, topicArn, endpoint) {
-  // TODO: Create params const
+function createSubscription(type, topicArn, endpoint) {
+  const params = {
+    Protocol: type,
+    TopicArn: topicArn,
+    Endpoint: endpoint,
+  };
 
   return new Promise((resolve, reject) => {
-    // TODO: Subscribe
-  })
+    sns.subscribe(params, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
 }
