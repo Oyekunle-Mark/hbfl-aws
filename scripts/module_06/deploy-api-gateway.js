02fed0fccdc66b72b4c51e6f10e8f9bfae9d4c4d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-13 16:44:35 +0100

    Deploys an api gateway

--- scripts/module_06/deploy-api-gateway.js
+++ scripts/module_06/deploy-api-gateway.js
@@ -1,19 +1,27 @@
 // Imports
-const AWS = require('aws-sdk')
+const AWS = require('aws-sdk');
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' });
 
 // Declare local variables
-const apiG = new AWS.APIGateway()
-const apiId = '/* TODO: Add api id */'
+const apiG = new AWS.APIGateway();
+const apiId = 'cqek2c5oek';
 
-createDeployment(apiId, 'prod')
-.then(data => console.log(data))
+createDeployment(apiId, 'prod').then((data) => console.log(data));
 
-function createDeployment (apiId, stageName) {
-  // TODO: Create params const
+function createDeployment(apiId, stageName) {
+  const params = {
+    restApiId: apiId,
+    stageName: stageName,
+  };
 
   return new Promise((resolve, reject) => {
-    // TODO: Create deployment
-  })
+    apiG.createDeployment(params, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
 }
