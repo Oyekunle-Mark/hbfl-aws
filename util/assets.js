27fde899fead15090135bfa6661dec143233ca8b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-13 17:12:49 +0100

    Updated the files path to point to the cloudfront domain

--- util/assets.js
+++ util/assets.js
@@ -1,4 +1,4 @@
-const base = 'https://hamster-bucket-chief.s3.amazonaws.com';
+const base = 'https://d1jsfl0mvzfc5o.cloudfront.net';
 module.exports = {
   hamster1: base + '/images/hamster-1-final.png',
   hamster2: base + '/images/hamster-2-final.png',
